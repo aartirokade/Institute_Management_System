d7af0c1d576acf7a0f5a520864262ed910f8aae7
aarti <aartirokade1996@gmail.com>
2021-09-30 17:56:17 +0530

    'projectuploaded'

---
+++ Institute_management_system_backend/src/main/java/com/app/InstituteManagmentSystemApplication.java
@@ -0,0 +1,15 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstituteManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Institute managament System" );
+		SpringApplication.run(InstituteManagmentSystemApplication.class, args);
+		System.out.println("Project Started..!!!");
+	}
+
+}
